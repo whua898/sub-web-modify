f0a5d0274e8baee2600d1167236d55cb16e5e456
whua898 <whua898@gmail.com>
2026-01-06 14:42:20 +0800

    wh

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -788,7 +788,7 @@ export default {
         sourceSubUrl: "",
         clientType: "",
         customBackend: this.getUrlParam() == "" ? defaultBackend : this.getUrlParam(),
-        shortType: "https://v1.mk/short",
+        shortType: "https://short.wh8.xx.kg/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
         includeRemarks: "",
