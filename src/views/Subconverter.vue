538f9a3b64892748a37082ed6f97d98b6bfcd5bf
whua898 <whua898@gmail.com>
2026-01-09 20:33:26 +0800

    wh

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -1792,7 +1792,7 @@ export default {
 .el-button--zhuti {
   position: relative;
   width: 54px !important;
-  height: 30px !important;
+  height: 20px !important;
   padding: 0 !important;
   display: flex;
   align-items: center;
