485dc0499e75d9b240c31ed367775dd2d48e4cb7
whua898 <whua898@gmail.com>
2026-01-09 20:36:41 +0800

    wh

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -1913,8 +1913,8 @@ export default {
 
 /* 调整主题切换按钮与相邻组件间距 */
 .theme-toggle-divider {
-  margin-top: 20px !important;
-  margin-bottom: 20px !important;
+  margin-top: 10px !important;
+  margin-bottom: 10px !important;
 }
 
 .advanced-item {
