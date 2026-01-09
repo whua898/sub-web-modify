99719cd1923f7c611a04bbf34cc87b4060b791b6
whua898 <whua898@gmail.com>
2026-01-09 17:10:27 +0800

    wh

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -152,7 +152,7 @@
                   </el-collapse-item>
                 </el-collapse>
               </el-form-item>
-              <el-divider content-position="center" style="margin-top: 0px !important; margin-bottom: 0px !important;">
+              <el-divider content-position="center" style="margin-top: -20px !important; margin-bottom: 0px !important;">
                 <el-button type="zhuti" @click="change">
                   <i id="rijian" class="el-icon-sunny"></i>
                   <i id="yejian" class="el-icon-moon"></i>
